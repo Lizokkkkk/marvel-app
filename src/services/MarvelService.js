bfb399c814ee0a81f64132877fbae6d4b3339988
Liza <liza.yemelyanova@list.ru>
2024-02-16 22:21:59 +0300

    add functional to CharList

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,6 +24,7 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'No character information',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
